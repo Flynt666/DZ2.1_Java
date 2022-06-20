ed6c996851dbf33c3338c0db26a889fa5f1fc13e
Борис Распутин <bang-kirov@bk.ru>
2022-06-20 19:03:43 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int pay = 950; // стоимость билета
+        int i = 20; // стоимость одной бонусной мили
+
+        System.out.println("Вы получили " + pay / i + " бонусных миль");
+
+
+    }
+
+}
